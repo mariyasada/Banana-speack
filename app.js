109d6c9a46e857d5688da92e7d2cbf11df4bde14
mariyasada <mariyasada2406@gmail.com>
2021-09-05 17:03:46 +0530

    adding function in js

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("welcome to javascript");
+
+var txtinput= document.querySelector("#text-input");
+var btntranslate = document.querySelector(".btn-translate");
+var txtoutput =document.querySelector("#text-output");
+
+ //var  serverUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+ var Url="https://api.funtranslations.com/translate/minion.json";
+
+
+ function constructUrl(text){
+     return Url + "?" + "text=" + text;
+ }
+
+ function errorHandler(){
+     console.log("error occured",error);
+     alert("something wrong with server! try again after some time");
+ }
+
+// Calling a function when button clicked
+btntranslate.addEventListener("click",() => {
+
+    var inputText = txtinput.value;
+    console.log("heyy");
+    fetch(constructUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        txtoutput.innerText = translatedText;
+
+    })
+    .catch(errorHandler)
+    console.log("hiiiiii");
+});
